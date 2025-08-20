3cee8c68fb20ad2e591b75f686dae5461358b954
wpplng <lina.wappling@gmail.com>
2025-08-20 13:23:19 +0200

    list section first version

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -1,6 +1,22 @@
 import type { ReactElement } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 
 const ListSection = (): ReactElement => {
-  return <section>ListSection</section>;
+  return (
+    <section className='list-section'>
+      <div className='song-info-box'>
+        <img
+          src='../src/assets/images/reputation.jpg'
+          className='list-section-image'
+        />
+        <div>
+          <h2>Taylor Swift</h2>
+          <h3>Reputation</h3>
+        </div>
+      </div>
+      <FontAwesomeIcon icon={faCirclePlay} />
+    </section>
+  );
 };
 export default ListSection;
